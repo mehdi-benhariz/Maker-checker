ea546b9fd14d402200bc2d7947638dd2d11687a2
mehdi-benhariz <benharizmehdi20@gmail.com>
2022-06-22 20:25:46 +0100

    first template

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityInfo.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGet("/", (context) => context.Response.WriteAsync("Hello Mehdi!"));
+
+app.Run();
